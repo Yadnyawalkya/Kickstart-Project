ae97c54ff9c6f4fa30e754483c7514d34e2ce536
Yadnyawalkya Tale <yadnyawalkyatale@gmail.com>
2017-04-07 00:06:29 +0530

    Version 0.7

--- two.py
+++ two.py
@@ -6,25 +6,8 @@ app = Flask(__name__)
 def index():
 	fo = open("kickstart.cfg", "ab+")
 	if request.method == 'POST':
-	###Display Configuration 
-		fo.write("\n# Use graphical install\ngraphical")
-		check_value = request.form.to_dict('display-checkbox')
-		check_or_uncheck = check_value['display-checkbox']
-		if check_or_uncheck =='check':
-			print "check"
-			first_boot = check_value['first-boot']
-			if first_boot == 'Enabled':
-				print "enable"
-				fo.write("\n# Run the Setup Agent on first boot\nfirstboot --enable")
-			elif first_boot == 'Disabled':
-				print "disable"
-                               	fo.write("\nfirstboot --disable")
-			elif first_boot == 'Reconfigure':
-				print "reconfigure"
-                                fo.write("\n# Run the Setup Agent on first boot\nfirstboot --reconfig")
-		elif check_or_uncheck == 'uncheck':
-			print "uncheck"	
-			fo.write("\n# Run the Setup Agent on first boot\nfirstboot --reconfig\n# Do not configure the X Window System\nskipx")
+		first_boot = request.form['first-boot']
+		print first_boot
 	return render_template('index.html')
 	fo.close()
 
